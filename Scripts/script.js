c6974e9b7f3128ac51372abbffabea59084e91de
Teet Simson <teet5858@gmail.com>
2021-03-20 16:17:52 +0200

    Basic design done

---
+++ Scripts/script.js
@@ -0,0 +1,26 @@
+function SunSelect() {
+    var x = document.getElementById("Sun").value;
+    console.log(x);
+
+    console.log(value);
+}
+
+function PumpSelect() {
+    var x = document.getElementById("Pump").value;
+    console.log(x);
+}
+
+function GasSelect() {
+    var x = document.getElementById("Gas").value;
+    console.log(x);
+}
+
+function OffGridSelect() {
+    var x = document.getElementById("OffGrid").value;
+    console.log(x);
+}
+
+function ElectricCarsSelect() {
+    var x = document.getElementById("ElectricCars").value;
+    console.log(x);
+}
