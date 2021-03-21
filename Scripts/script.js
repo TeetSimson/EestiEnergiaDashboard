11589eec32b946fd9af45fac973da7c4ef6baf3d
Teet Simson <teet5858@gmail.com>
2021-03-21 04:34:49 +0200

    dropdown change broken

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,26 +1,317 @@
-function SunSelect() {
-    var x = document.getElementById("Sun").value;
-    console.log(x);
+function GetValues() {
+    console.log(sliderValue);
 
-    console.log(value);
+}
+
+function SunSelect() {
+    checkbox = 'Sun';
+    periodValue = document.getElementById("PeriodDrop").value = 60;
+    onInputChange();
 }
 
 function PumpSelect() {
-    var x = document.getElementById("Pump").value;
-    console.log(x);
+    checkbox = 'Pump';
+    periodValue = document.getElementById("PeriodDrop").value = 48;
+    onInputChange();
 }
 
 function GasSelect() {
-    var x = document.getElementById("Gas").value;
-    console.log(x);
+    checkbox = 'Gas';
+    periodValue = document.getElementById("PeriodDrop").value = 48;
+    onInputChange();
 }
 
 function OffGridSelect() {
-    var x = document.getElementById("OffGrid").value;
-    console.log(x);
+    checkbox = 'OffGrid';
+    periodValue = document.getElementById("PeriodDrop").value = 60;
+    onInputChange();
 }
 
 function ElectricCarsSelect() {
-    var x = document.getElementById("ElectricCars").value;
-    console.log(x);
+    checkbox = 'ElectricCars';
+    periodValue = document.getElementById("PeriodDrop").value = 48;
+    onInputChange();
+}
+
+function onInputChange() {
+
+    // Selectors
+    const intrest = document.getElementById("Intrest");
+    const fee = document.getElementById("Fee");
+    const monthlyFee = document.getElementById("MonthlyFee");
+    const period = document.getElementById("Period");
+    const projectCost = document.getElementById("ProjectCost");
+    const monthlyPayment = document.getElementById("MonthlyPayment");
+    const list = document.getElementById("PeriodDrop");
+
+    let periodValue = parseInt(document.getElementById("PeriodDrop").value);
+    let deposit = document.getElementById("DepositInput").value;
+    let projectCostValue = parseInt(sliderValue);
+    if (projectCostValue == 25) projectCostValue = 25000;
+
+    if (deposit === '') // If no deposit calculate with 0
+        deposit = 0;
+    else if (deposit > projectCostValue-300) deposit = 0; // 300 min total
+    else if (deposit < 0) deposit = 0;
+    else parseInt(deposit);
+
+    projectCostValue -= deposit;
+
+    // Check which service is checked
+
+    // PV
+    if (checkbox == 'Sun') {
+        if (hidden) {
+            for (var i=0; i<8; i++) {
+                opt = document.createElement("OPTION");
+                opt.setAttribute("value",78+i*6);
+                let text = document.createTextNode(78+i*6 + " kuud");
+                opt.appendChild(text);
+                document.getElementById("PeriodDrop").appendChild(opt); 
+            }
+            hidden = false;
+        }
+
+        intrest.textContent = '7,90 %';
+        fee.textContent = '50 €';
+        monthlyFee.textContent = '1 €';
+        period.textContent = periodValue + ' kuud';
+        projectCost.textContent = projectCostValue;
+
+        let monthlyPaymentValue = parseFloat(((projectCostValue * 1.079) / periodValue) + 50 + periodValue);
+        monthlyPayment.textContent = monthlyPaymentValue.toFixed(2);
+    
+    // Soojuspumbad
+    } else if (checkbox == 'Pump') {
+        if (projectCostValue < 2000) {
+            if (hidden === false){
+                // Change dropdown
+                for (var i=0; i<16; i++) {  
+                    list.removeChild(list.lastChild); 
+                }
+                hidden = true;
+            }
+
+            intrest.textContent = '7,90 %';
+            fee.textContent = '30 €';
+            perc = 1.079;
+            contractFee = 30;
+        }
+        else if (projectCostValue < 6000) {
+            if (hidden === false){
+                // Change dropdown
+                for (var i=0; i<16; i++) {  
+                    list.removeChild(list.lastChild); 
+                }
+                hidden = true;
+            }
+            intrest.textContent = '6,50 %';
+            fee.textContent = '20 €';
+            perc = 1.065;
+            contractFee = 20;
+        }
+        else {
+            if (hidden) {
+                for (var i=0; i<8; i++) {
+                    opt = document.createElement("OPTION");
+                    opt.setAttribute("value",78+i*6);
+                    let text = document.createTextNode(78+i*6 + " kuud");
+                    opt.appendChild(text);
+                    document.getElementById("PeriodDrop").appendChild(opt); 
+                }
+                hidden = false;
+            }
+
+            intrest.textContent = '5,90 %';
+            fee.textContent = '20 €';
+            perc = 1.059;
+            contractFee = 20;
+        }
+        monthlyFee.textContent = '1 €';
+        period.textContent = periodValue + ' kuud';
+        projectCost.textContent = projectCostValue;
+
+        let monthlyPaymentValue = parseFloat(((projectCostValue * perc) / periodValue) + contractFee + periodValue);
+        monthlyPayment.textContent = monthlyPaymentValue.toFixed(2);
+
+    // Elektriautode laadijad
+    } else if (checkbox == 'ElectricCars') {
+        if (projectCostValue < 2000) {
+            if (hidden === false){
+                // Change dropdown
+                for (var i=0; i<16; i++) {  
+                    list.removeChild(list.lastChild); 
+                }
+                hidden = true;
+            }
+
+            intrest.textContent = '5,90 %';
+            fee.textContent = '50 €';
+            perc = 1.059;
+            contractFee = 50;
+        }
+        else if (projectCostValue < 6000) {
+            if (hidden === false){
+                // Change dropdown
+                for (var i=0; i<16; i++) {  
+                    list.removeChild(list.lastChild); 
+                }
+                hidden = true;
+            }
+
+            intrest.textContent = '4,50 %';
+            fee.textContent = '40 €';
+            perc = 1.045;
+            contractFee = 40;
+        }
+        else {
+            if (hidden) {
+                for (var i=0; i<8; i++) {
+                    opt = document.createElement("OPTION");
+                    opt.setAttribute("value",78+i*6);
+                    let text = document.createTextNode(78+i*6 + " kuud");
+                    opt.appendChild(text);
+                    document.getElementById("PeriodDrop").appendChild(opt); 
+                }
+                hidden = false;
+            }
+
+            intrest.textContent = '3,90 %';
+            fee.textContent = '30 €';
+            perc = 1.039;
+            contractFee = 30;
+        }
+        monthlyFee.textContent = '2 €';
+        period.textContent = periodValue + ' kuud';
+        projectCost.textContent = projectCostValue;
+
+        let monthlyPaymentValue = parseFloat(((projectCostValue * perc) / periodValue) + contractFee + (2*periodValue));
+        monthlyPayment.textContent = monthlyPaymentValue.toFixed(2);
+
+    // Võrguvaba elektrijaam Off-grid
+    } else if (checkbox == 'OffGrid') {
+        if (projectCostValue < 2000) {
+            if (hidden === false){
+                // Change dropdown
+                for (var i=0; i<16; i++) {  
+                    list.removeChild(list.lastChild); 
+                }
+                hidden = true;
+            }
+
+            intrest.textContent = '7,90 %';
+            fee.textContent = '30 €';
+            perc = 1.059;
+            contractFee = 30;
+        }
+        else if (projectCostValue < 6000) {
+            if (hidden === false){
+                // Change dropdown
+                for (var i=0; i<16; i++) {  
+                    list.removeChild(list.lastChild); 
+                }
+                hidden = true;
+            }
+
+            intrest.textContent = '6,50 %';
+            fee.textContent = '20 €';
+            perc = 1.045;
+            contractFee = 20;
+        }
+        else {
+            intrest.textContent = '5,90 %';
+            fee.textContent = '20 €';
+            perc = 1.039;
+            contractFee = 20;
+
+            if (hidden) {
+                for (var i=0; i<8; i++) {
+                    opt = document.createElement("OPTION");
+                    opt.setAttribute("value",78+i*6);
+                    let text = document.createTextNode(78+i*6 + " kuud");
+                    opt.appendChild(text);
+                    document.getElementById("PeriodDrop").appendChild(opt); 
+                }
+                hidden = false;
+            }
+        }
+        monthlyFee.textContent = '0 €';
+        period.textContent = periodValue + ' kuud';
+        projectCost.textContent = projectCostValue;
+
+        let monthlyPaymentValue = parseFloat(((projectCostValue * perc) / periodValue) + contractFee);
+        monthlyPayment.textContent = monthlyPaymentValue.toFixed(2);
+
+    // Elektri- ja gaasitööd     
+    } else if (checkbox == 'Gas') {
+        if (projectCostValue < 1000) {
+            if (hidden === false){
+                // Change dropdown
+                for (var i=0; i<16; i++) {  
+                    list.removeChild(list.lastChild); 
+                }
+                hidden = true;
+            }
+
+            intrest.textContent = '9,90 %';
+            fee.textContent = '30 €';
+            perc = 1.099;
+            contractFee = 30 + periodValue;
+            monthlyFee.textContent = '1 €';
+        } 
+        else if (projectCostValue < 2000) {
+            if (hidden === false){
+                // Change dropdown
+                for (var i=0; i<16; i++) {  
+                    list.removeChild(list.lastChild); 
+                }
+                hidden = true;
+            }
+
+            intrest.textContent = '8,90 %';
+            fee.textContent = '20 €';
+            perc = 1.089;
+            contractFee = 20 + periodValue;
+            monthlyFee.textContent = '1 €';
+        }
+        else if (projectCostValue < 6000) {
+            if (hidden === false){
+                // Change dropdown
+                for (var i=0; i<16; i++) {  
+                    list.removeChild(list.lastChild); 
+                }
+                hidden = true;
+            }
+
+            intrest.textContent = '7,50 %';
+            fee.textContent = '20 €';
+            perc = 1.075;
+            contractFee = 20;
+            monthlyFee.textContent = '0 €';
+        }
+        else {
+            intrest.textContent = '6,90 %';
+            fee.textContent = '20 €';
+            perc = 1.069;
+            contractFee = 20;
+            monthlyFee.textContent = '0 €';
+
+            if (hidden) {
+                for (var i=0; i<8; i++) {
+                    opt = document.createElement("OPTION");
+                    opt.setAttribute("value",78+i*6);
+                    let text = document.createTextNode(78+i*6 + " kuud");
+                    opt.appendChild(text);
+                    document.getElementById("PeriodDrop").appendChild(opt); 
+                }
+                hidden = false;
+            }
+        }
+        period.textContent = periodValue + ' kuud';
+        projectCost.textContent = projectCostValue;
+
+        let monthlyPaymentValue = parseFloat(((projectCostValue * perc) / periodValue) + contractFee);
+        monthlyPayment.textContent = monthlyPaymentValue.toFixed(2);
+    }
+    
 }
